f98f3ed4bd55b2bce08116a02201d25987e43723
AlexqCODE <Sasha2018_0000>
2025-11-18 15:07:02 +0000

    init lab work

---
+++ Calculator.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Лабораторная работа №1 - Работа с Git");
+            Calculator calculator = new Calculator();
+
+            double result = calculator.Add(5, 3);
+            Console.WriteLine($"5 + 3 = {result}");
+        }
+    }
+}
