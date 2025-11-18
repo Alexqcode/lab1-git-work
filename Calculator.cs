2b7819ce0d26c8ed4045f7e23e3fed2fc7caccdf
AlexqCODE <Sasha2018_0000>
2025-11-18 15:13:52 +0000

    feat: добавлен калькулятор

--- Calculator.cs
+++ Calculator.cs
@@ -6,5 +6,10 @@ namespace ConsoleApp1
         {
             return a + b;
         }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
     }
 }
