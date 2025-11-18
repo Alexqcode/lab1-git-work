a6923c4f29fee4bf4bf8fb92b6ad6a654f6f925c
AlexqCODE <Sasha2018_0000>
2025-11-18 15:45:02 +0000

    feat: добавлен функционал в Calculate.cs

--- Calculator.cs
+++ Calculator.cs
@@ -6,5 +6,15 @@ namespace ConsoleApp1
         {
             return a + b;
         }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
     }
 }
